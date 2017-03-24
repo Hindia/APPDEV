e5fc3ae1d3e64e6881dfefdf92c313bc94dc1111
Hindia <mahmmudhindia@gmail.com>
2017-03-24 11:17:21 +0000

    comm.h

---
+++ comm.h
@@ -0,0 +1,6 @@
+//we specify necessary constants for curl communication here
+#define COMM
+#define URL "http://www.cc.puv.fi/~e1500943/php/sound.php"
+
+//function protype 
+void send_data_curl(double []);
