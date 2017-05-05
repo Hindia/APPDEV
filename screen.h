ccdffdac42c59e39503e77c5b692d17822efc732
Hindia <mahmmudhindia@gmail.com>
2017-05-05 13:25:52 +0000

    Final

--- screen.h
+++ screen.h
@@ -1,11 +1,11 @@
 //includes the function prtotypes for screen manipulation
-enum COLOR{BLACK=30, RED, GREEN, YELLOW, BLUE, MAGNETA,CYAN, WHITE};
-#define UNICODE
+enum COLOR{BLACK=30, RED, GREEN, YELLOW, BLUE, MAGNETA,CYAN, WHITE};	//enumeration for the colors to be used in the screen.c
+#define UNICODE	//used in the screen.c for debug mode
 #define BAR "\u2590" //unicoe for bar
 
 
 
-//function prototypes
+//function prototypes defined in the screen.c file
 void setFGcolor(int fg);
 void resetColors(void);
 void gotoXY(int row, int col);
