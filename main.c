40b4666529e7398e7302c856b6f63b27628133c8
Hindia <mahmmudhindia@gmail.com>
2017-05-05 13:16:50 +0000

    Final

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+//headers included
 #include "wave.h"
 #include "screen.h"
 #include <stdio.h>
@@ -9,22 +10,22 @@
 //this program will open a wave file, and display WAV header info if DEBUG is enabled
 
 int main(int argc, char *argv[]){
-	FILE *fp;
-	WAVHDR myhdr;
-	short int sa[SAMPLE_RATE];
+	FILE *fp;	//file pointer
+	WAVHDR myhdr;	//header
+	short int sa[SAMPLE_RATE];	//array for samples
 	int answer, ret;
-	for(;;){	//starts a never ending loop)
-	  	ret=system("arecord -r16000 -c1 -d1 -f S16_LE -q data.wav");
-	  	if(WIFSIGNALED(ret) && (WTERMSIG(ret)==SIGINT || WTERMSIG(ret)==SIGQUIT))break;
-		clearScreen();
-	  	fp=fopen("data.wav", "r");
-	  	fread(&myhdr, sizeof(myhdr), 1, fp);
-	  	displayWAVHDR(myhdr);
-          	fread(&sa, sizeof(short int), SAMPLE_RATE, fp);
-		displayWAVdata(sa);
-		fclose(fp);
+	for(;;){	//starts a never ending loop
+	  	ret=system("arecord -r16000 -c1 -d1 -f S16_LE -q data.wav");	//returns the recording command
+	  	if(WIFSIGNALED(ret) && (WTERMSIG(ret)==SIGINT || WTERMSIG(ret)==SIGQUIT))break;	//if a terminating signal is hit the program stops running
+		clearScreen();	//clears screen
+	  	fp=fopen("data.wav", "r");	//opens to read the file
+	  	fread(&myhdr, sizeof(myhdr), 1, fp);	//reads the header
+	  	displayWAVHDR(myhdr);	//displays the header
+          	fread(&sa, sizeof(short int), SAMPLE_RATE, fp);	//reads the sample rate
+		displayWAVdata(sa);	//displays the wave data
+		fclose(fp);	//closes the file
 	}
-	printf("wanna generate a test tone?(1: yes, 0:no)\n");
-	scanf("%d", &answer);
-	if(answer==1)TestTone(440, 5);
+	printf("wanna generate a test tone?(1: yes, 0:no)\n");	//gives an option to user if they wish to generate a test  tone
+	scanf("%d", &answer);	//gets the response from user
+	if(answer==1)TestTone(440, 5);	//generates a test tone if the user wishes to
 }
