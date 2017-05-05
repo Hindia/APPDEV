3784255a56020c7406da8c6a95f5497a9e52339f
Hindia <mahmmudhindia@gmail.com>
2017-05-05 13:02:08 +0000

    Final

--- comm.h
+++ comm.h
@@ -1,7 +1,7 @@
 //we specify necessary constants for curl communication here
 
-#define COMM
-#define URL "http://www.cc.puv.fi/~e1500943/php/sound.php"
+#define COMM	//used for conditional debugging in other codes in the program
+#define URL "http://www.cc.puv.fi/~e1500943/php/sound.php"	//the URL is defined here
 
-//function protype 
+//function protype defined for comm.c
 void send_data_curl(double []);
