a077b7964ab07a7f589ff68f2f39b8584899adae
Hindia <mahmmudhindia@gmail.com>
2017-05-05 12:59:08 +0000

    Final

--- comm.c
+++ comm.c
@@ -1,30 +1,30 @@
-#include "comm.h"
-#include <stdio.h>
-#include <curl/curl.h>
-#include <string.h>
+#include "comm.h"	//includes the header file of communication
+#include <stdio.h>	//includes the standard input output library
+#include <curl/curl.h>	//includes the curl library
+#include <string.h>	//includes the string library to use strings in this code
 
 //send_data_curl function defined
 void send_data_curl(double Leq[]){
-	char postdata[200]="data=", temp[30];
-	int i;
-	CURL *curl;
+	char postdata[200]="data=", temp[30];	//array to hold the sample recordings
+	int i;	//variable for looping
+	CURL *curl;	//pointer defined
 	CURLcode res;
+	//prepares the postdata
 	for(i=0;i<8;i++){
-		sprintf(temp,(i==7)?"%.2f":"%.2f;",Leq[i]);
+		sprintf(temp,(i==7)?"%.2f":"%.2f;",Leq[i]);	//gives format the LEQ values and gives it to temp
 		strcat(postdata, temp);	//appends temp to postdata
 	}
-//	printf("%s\n", postdata);
-	curl_global_init(CURL_GLOBAL_ALL);
+	curl_global_init(CURL_GLOBAL_ALL);	//initializes  all curl
 	curl = curl_easy_init();
 	if(curl){
 		 //set URL of server page
 		curl_easy_setopt(curl, CURLOPT_URL, URL);
-	 	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata); 
-	 	res=curl_easy_perform(curl);	 //perform a submit
-		if(res!= CURLE_OK){	 //if server side is negative
+	 	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata);	//sends the postdata value to the post fields of curl 
+	 	res=curl_easy_perform(curl);	 //performs a submit
+		if(res!= CURLE_OK){	 //checks if server side is negative
 	 		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));	//output error
 		}
 		curl_easy_cleanup(curl); 	 //clean up handler
-	}	
+	}
 	curl_global_cleanup();	 //globally ended curl
 }
